db4f15ef69c6b5e5fe261b978a09cbc8b835772a
Benjamin Storm <dev@benjaminstorm.com>
2026-02-13 14:32:07 +0100

    Reverse the folder of go and python

--- python/minitwit_tests.py
+++ python/minitwit_tests.py
@@ -8,7 +8,7 @@
     :copyright: (c) 2010 by Armin Ronacher.
     :license: BSD, see LICENSE for more details.
 """
-import minitwit
+import old.minitwit as minitwit
 import unittest
 import tempfile
 
