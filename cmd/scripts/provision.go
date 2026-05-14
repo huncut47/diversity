75c7aa4e4cc444b972498ca6a270ea8a49766c53
Benjamin Storm <dev@benjaminstorm.com>
2026-05-14 13:09:13 +0200

    feat: firewall as iaC
    - Automatic creation of firewall
    - Attachment to Hetzner server

--- cmd/scripts/provision.go
+++ cmd/scripts/provision.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"log"
 	"log/slog"
+	"net"
 	"os"
 
 	"github.com/hetznercloud/hcloud-go/v2/hcloud"
@@ -39,12 +40,20 @@ func main() {
 		return
 	}
 
+	// Create firewall
+	firewall, err := createFirewall(ctx, client, "Twititu Firewall")
+	if err != nil {
+		logger.Error("error creating firewall", slog.String("error", err.Error()))
+		return
+	}
+
 	result, _, err := client.Server.Create(ctx, hcloud.ServerCreateOpts{
 		Name:       "Twititu",
 		Image:      &hcloud.Image{Name: "ubuntu-24.04"},
 		ServerType: &hcloud.ServerType{Name: "cx23"},
 		Location:   &hcloud.Location{Name: "fsn1"},
 		SSHKeys:    []*hcloud.SSHKey{sshKey},
+		Firewalls:  []*hcloud.ServerCreateFirewall{{Firewall: *firewall}},
 	})
 	if err != nil {
 		logger.Error("error creating server", slog.String("error", err.Error()))
@@ -80,4 +89,67 @@ func getSSHKeyByName(ctx context.Context, client *hcloud.Client, name string) (*
 		return nil, nil
 	}
 	return sshKeys[0], nil
+}
+
+func createFirewall(ctx context.Context, client *hcloud.Client, name string) (*hcloud.Firewall, error) {
+	httpDescription := "Allow HTTP"
+	httpPort := "80"
+	httpsDescription := "Allow HTTPS"
+	httpsPort := "443"
+	grafanaDescription := "Allow Grafana"
+	grafanaPort := "3001"
+	kibanaDescription := "Allow Kibana"
+	kibanaPort := "5601"
+	sshDescription := "Allow SSH"
+	sshPort := "22"
+
+	allIPs := []net.IPNet{
+		{IP: net.IPv4(0, 0, 0, 0), Mask: net.CIDRMask(0, 32)},
+		{IP: net.IP{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}, Mask: net.CIDRMask(0, 128)},
+	}
+
+	firewall, _, err := client.Firewall.Create(ctx, hcloud.FirewallCreateOpts{
+		Name: name,
+		Rules: []hcloud.FirewallRule{
+			{
+				Description: &httpDescription,
+				Direction:   hcloud.FirewallRuleDirectionIn,
+				Protocol:    hcloud.FirewallRuleProtocolTCP,
+				Port:        &httpPort,
+				SourceIPs:   allIPs,
+			},
+			{
+				Description: &httpsDescription,
+				Direction:   hcloud.FirewallRuleDirectionIn,
+				Protocol:    hcloud.FirewallRuleProtocolTCP,
+				Port:        &httpsPort,
+				SourceIPs:   allIPs,
+			},
+			{
+				Description: &grafanaDescription,
+				Direction:   hcloud.FirewallRuleDirectionIn,
+				Protocol:    hcloud.FirewallRuleProtocolTCP,
+				Port:        &grafanaPort,
+				SourceIPs:   allIPs,
+			},
+			{
+				Description: &kibanaDescription,
+				Direction:   hcloud.FirewallRuleDirectionIn,
+				Protocol:    hcloud.FirewallRuleProtocolTCP,
+				Port:        &kibanaPort,
+				SourceIPs:   allIPs,
+			},
+			{
+				Description: &sshDescription,
+				Direction:   hcloud.FirewallRuleDirectionIn,
+				Protocol:    hcloud.FirewallRuleProtocolTCP,
+				Port:        &sshPort,
+				SourceIPs:   allIPs,
+			},
+		},
+	})
+	if err != nil {
+		return nil, err
+	}
+	return firewall.Firewall, nil
 }
